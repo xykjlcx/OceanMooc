0f09ef82d0219443a1309714ff6f9475ffec138d
Ocean.Li <348686686@qq.com>
2018-06-15 14:35:16 +0800

    依赖关系调整

--- example/src/main/java/com/oceanli/oceanmooc/example/MainActivity.java
+++ example/src/main/java/com/oceanli/oceanmooc/example/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.oceanli.ocean.core.OceanUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("ocean", "onCreate: " + "项目初始化完成");
+        Log.e("ocean", "onCreate: " + "项目初始化完成" + "打印:***" + OceanUtils.AUTHOR + "***");
     }
 }
---
+++ ocean-core/src/main/java/com/oceanli/ocean/core/OceanUtils.java
@@ -0,0 +1,11 @@
+package com.oceanli.ocean.core;
+
+/**
+ * Created by haiyang on 2018/6/15.
+ */
+
+public class OceanUtils {
+
+    public static final String AUTHOR = "Ocean.Li";
+
+}
