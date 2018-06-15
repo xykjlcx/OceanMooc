5a8ffea4dc0cda6d9c58db01f8303d72c305b394
Ocean.Li <348686686@qq.com>
2018-06-15 11:15:39 +0800

    初始化完成

--- example/src/main/java/com/oceanli/oceanmooc/example/MainActivity.java
+++ example/src/main/java/com/oceanli/oceanmooc/example/MainActivity.java
@@ -1,7 +1,8 @@
 package com.oceanli.oceanmooc.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ocean", "onCreate: " + "项目初始化完成");
     }
 }
